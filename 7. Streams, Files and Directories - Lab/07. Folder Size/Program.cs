72d7a46a27d030d2cb22ea5a9bd88d432727b86a
DestroyerBg <gabriel-kulev@abv.bg>
2023-10-01 21:13:10 +0300

    All Exercices

--- 7. Streams, Files and Directories - Lab/07. Folder Size/Program.cs
+++ 7. Streams, Files and Directories - Lab/07. Folder Size/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            decimal sum = 0;
+            DirectoryInfo getDirectories = new DirectoryInfo(folderPath);
+            FileInfo[] files = getDirectories.GetFiles("*", SearchOption.AllDirectories);
+            foreach (FileInfo file in files)
+            {
+                sum += file.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+           File.WriteAllText(outputFilePath,$"{sum} KB");
+        }
+    }
+}
