d508fc4ffeaf3d717dca5bd03c875f8231216343
DestroyerBg <gabriel-kulev@abv.bg>
2023-10-13 13:39:42 +0300

    Workshop CustomDoublyLinkedList

---
+++ 13.Custom Doubly Linked List/CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private class ListNode
+        {
+            public int Value { get; set; }
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+
+            public ListNode(int value)
+            {
+                Value = value;
+            }
+
+        }
+        private ListNode Head { get; set; }
+        private ListNode Tail { get; set; }
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            if (Count == 0)
+            {
+                Tail = Head = new ListNode(element) ;
+            }
+            else
+            {
+                var newHead = new ListNode(element);
+                newHead.NextNode = Head;
+                Head.PreviousNode = newHead;
+                Head = newHead;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            if (Count == 0)
+            {
+                Tail = Head = new ListNode(element);
+            }
+            else
+            {
+                var newTail = new ListNode(element);
+                newTail.PreviousNode = Tail;
+                Tail.NextNode = newTail;
+                Tail = newTail;
+            }
+        }
+
+        public int RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+            var firstElement = Head.Value;
+            Head = Head.NextNode;
+            if (Head != null)
+            {
+                Head.PreviousNode = null;
+            }
+            else
+            {
+                Tail = null;
+            }
+
+            Count--;
+            return firstElement;
+        }
+
+        public int RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+            var lastElement = Tail.Value;
+            Tail = Tail.PreviousNode;
+            if (Tail != null)
+            {
+                Tail.NextNode = null;
+            }
+            else
+            {
+                Head = null;
+            }
+
+            Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            var currNode = Head;
+            while (currNode!=null)
+            {
+                action(currNode.Value);
+                currNode = currNode.NextNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+            int counter = 0;
+            var currNode = Head;
+            while (currNode != null)
+            {
+                array[counter++] = currNode.Value;
+                currNode = currNode.NextNode;
+            }
+            return array;
+        }
+    }
+}
---
+++ 13.Custom Doubly Linked List/CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Channels;
+using CustomDoublyLinkedList;
+
+var newList = new DoublyLinkedList();
+
+for (int i = 5; i >= 1; i--)
+{
+    newList.AddFirst(i); //Добавяме 1,2,3,4,5
+}
+
+newList.ForEach(x=>Console.Write($"{x} ")); // Печатаме
+Console.WriteLine();
+Console.WriteLine($"-------------------"); 
+int last = newList.RemoveLast();
+Console.WriteLine(last); // Изтриваме последния елемент от листа и го печатаме
+Console.WriteLine($"-------------------");
+int first = newList.RemoveFirst();
+Console.WriteLine(first); // Изтриваме първия елемент от листа и го печатаме
+Console.WriteLine($"-------------------");
+int count =  newList.Count; 
+Console.WriteLine(count);  // Колко числа имаме в листа до момента?
+Console.WriteLine($"-------------------");
+int[] toArrayTest = newList.ToArray();
+Console.WriteLine(string.Join(" ",toArrayTest)); // Правим масив от листа и го печатаме със StringJoin
