01dad94bd3ea0491fc8fe50fa8ca031d2dff41f1
DestroyerBg <gabriel-kulev@abv.bg>
2023-10-14 11:25:16 +0300

    All Exercices

---
+++ 15.GenericsLab/03.GenericScale/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> equal = new EqualityScale<int>(2, 3);
+            Console.WriteLine(equal.AreEqual());
+        }
+    }
+}
