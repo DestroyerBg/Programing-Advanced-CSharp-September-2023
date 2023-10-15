9a84aef37c24c76bb4efa0ee508dd420c82d2c11
DestroyerBg <gabriel-kulev@abv.bg>
2023-10-15 13:29:28 +0300

    All Exercices

---
+++ 17.IteratorsАndComparatorsLab/01.Library/Library.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int index = -1;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+            }
+
+            public bool MoveNext()
+            {
+                index++;
+                return index < books.Count;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+
+            public Book Current => books[index];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+            }
+
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    }
+}
